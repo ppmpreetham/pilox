cb9363c9694533fa45ef8e0052a17dafd46d65ad
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2025-05-09 22:28:58 +0530

    add: scanner

--- language/scanner.py
+++ language/scanner.py
@@ -1,12 +1,16 @@
 from typing import List, Tuple, Union, Optional
-from language.TokenType import *
+from TokenType import *
 
 class Scanner:
     
     tokens: Token = []
+    start: int = 0
+    current: int = 0
+    line: int = 1
+    source = ""
     
     def __init__(self, source: str):
-        """scans a given source code and returns a list of tokens.
+        """Scans a given source code and returns a list of tokens.
 
         Args:
             source (str): the source code to be scanned
@@ -14,7 +18,7 @@ class Scanner:
         self.source = source
         
     def scanTokens(self) -> List[Token]:
-        """scans the tokens in the source code.
+        """Scans the tokens in the source code.
         
         Returns:
             List[Token]: a list of tokens found in the source code
@@ -27,7 +31,7 @@ class Scanner:
             self.start = self.current
             self.scanToken()
         
-        self.tokens.append(Token(TokenType.EOF, "", None, 0))
+        self.tokens.append(Token(TokenType.EOF, "", None, self.line))
         
         return self.tokens
         
@@ -39,3 +43,35 @@ class Scanner:
         """
         return self.current >= len(self.source)
     
+    def advance(self):
+        self.current += 1
+        return self.source[self.current-1]
+    
+    
+    def scanToken(self):
+        c = self.advance()
+        match c:
+            case '(':
+                self.addToken(TokenType.LEFT_PAREN)
+            case ')':
+                self.addToken(TokenType.RIGHT_PAREN)
+            case '{':
+                self.addToken(TokenType.LEFT_BRACE)
+            case '}':
+                self.addToken(TokenType.RIGHT_BRACE)
+            case ',':
+                self.addToken(TokenType.COMMA)
+            case '.':
+                self.addToken(TokenType.DOT)
+            case '-':
+                self.addToken(TokenType.MINUS)
+            case '+':
+                self.addToken(TokenType.PLUS)
+            case ';':
+                self.addToken(TokenType.SEMICOLON)
+            case '*':
+                self.addToken(TokenType.STAR)
+                
+    def addToken(self, type: TokenType, literal: object):
+        text = self.source[self.start : self.current]
+        self.tokens.append(Token(type, text, literal, self.line))
