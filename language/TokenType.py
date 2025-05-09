4e788ee85aa4c961e436d39a1817fea8361ccb3a
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2025-05-09 22:29:06 +0530

    fix: tokens

--- language/TokenType.py
+++ language/TokenType.py
@@ -1,17 +1,54 @@
 from enum import Enum, auto
 
+from enum import Enum, auto
+
 class TokenType(Enum):
     # Single-character tokens
-    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE, COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR = auto()
+    LEFT_PAREN = auto()
+    RIGHT_PAREN = auto()
+    LEFT_BRACE = auto()
+    RIGHT_BRACE = auto()
+    COMMA = auto()
+    DOT = auto()
+    MINUS = auto()
+    PLUS = auto()
+    SEMICOLON = auto()
+    SLASH = auto()
+    STAR = auto()
     
     # One or two character tokens
-    BANG, BANG_EQUAL, EQUAL, EQUAL_EQUAL, GREATER, GREATER_EQUAL, LESS, LESS_EQUAL = auto()
+    BANG = auto()
+    BANG_EQUAL = auto()
+    EQUAL = auto()
+    EQUAL_EQUAL = auto()
+    GREATER = auto()
+    GREATER_EQUAL = auto()
+    LESS = auto()
+    LESS_EQUAL = auto()
     
     # Literals
-    IDENTIFIER, STRING, NUMBER = auto()
+    IDENTIFIER = auto()
+    STRING = auto()
+    NUMBER = auto()
     
     # Keywords
-    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR, PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE, EOF = auto()
+    AND = auto()
+    CLASS = auto()
+    ELSE = auto()
+    FALSE = auto()
+    FUN = auto()
+    FOR = auto()
+    IF = auto()
+    NIL = auto()
+    OR = auto()
+    PRINT = auto()
+    RETURN = auto()
+    SUPER = auto()
+    THIS = auto()
+    TRUE = auto()
+    VAR = auto()
+    WHILE = auto()
+    EOF = auto()
 
 class Token:
     """A class representing a token in the source code."""
